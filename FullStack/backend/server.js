03e76a5e10658b4ea50f69a60d998635be58bea3
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-04-03 00:18:09 +0530

    git backend

--- FullStack/backend/server.js
+++ FullStack/backend/server.js
@@ -7,6 +7,39 @@ app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
+// get a list of 5 jokes
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A scarecrow joke',
+            joke: 'Why did the scarecrow win an award? Because he was outstanding in his field.'
+        },
+        {
+            id: 2,
+            title: 'A bicycle joke',
+            joke: 'Why did the bicycle fall over? Because it was two tired.'
+        },
+        {
+            id: 3,
+            title: 'A belt joke',
+            joke: 'What do you call a belt made out of watches? A waist of time.'
+        },
+        {
+            id: 4,
+            title: 'A math book joke',
+            joke: 'Why did the math book look sad? Because it had too many problems.'
+        },
+        {
+            id: 5,
+            title: 'A fish joke',
+            joke: 'What do you call a fish wearing a crown? A king fish.'
+        }
+    ];
+    
+    res.send(jokes);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
