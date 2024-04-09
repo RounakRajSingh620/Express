c32b080e9c34b10ab232c91b5134fe73b84ce1c9
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-04-09 19:57:52 +0530

    Frontend/FullStack

--- FullStack/frontend/src/App.jsx
+++ FullStack/frontend/src/App.jsx
@@ -1,10 +1,21 @@
+/* eslint-disable no-unused-vars */
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [jokes, setJokes] = useState([]);
 
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/jokes')
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data', error);
+      });
+  });
   return (
     <>
       <h1> Chai and FullStack </h1>
