f3a22e4e34e64b9fe465b9cae4a0fd42dc081b5b
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-04-12 13:23:44 +0530

    Backend/FullStack

--- FullStack/backend/server.js
+++ FullStack/backend/server.js
@@ -1,51 +1,46 @@
 import express from 'express';
-// const cors = require('cors');
+
 const app = express();
-
-// app.use(cors({
-//     origin: 'http://localhost:5173' // Allow requests from this origin only
-// }));
-
 
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
 // get a list of 5 jokes
-app.get('/api/jokes', (req, res) => {
+app.get("/api/jokes", (req, res) => {
     const jokes = [
         {
             id: 1,
-            title: 'A scarecrow joke',
-            joke: 'Why did the scarecrow win an award? Because he was outstanding in his field.'
+            title: " A scarecrow joke",
+            joke: "Why did the scarecrow win an award? Because he was outstanding in his field."
         },
         {
             id: 2,
-            title: 'A bicycle joke',
-            joke: 'Why did the bicycle fall over? Because it was two tired.'
+            title: "A bicycle joke",
+            joke: "Why did the bicycle fall over? Because it was two tired."
         },
         {
             id: 3,
-            title: 'A belt joke',
-            joke: 'What do you call a belt made out of watches? A waist of time.'
+            title: "A belt joke",
+            joke: "What do you call a belt made out of watches? A waist of time."
         },
         {
             id: 4,
-            title: 'A math book joke',
-            joke: 'Why did the math book look sad? Because it had too many problems.'
+            title: "A math book joke",
+            joke: "Why did the math book look sad? Because it had too many problems."
         },
         {
             id: 5,
-            title: 'A fish joke',
-            joke: 'What do you call a fish wearing a crown? A king fish.'
+            title: "A fish joke",
+            joke: "What do you call a fish wearing a crown? A king fish."
         },
         {
             id: 6,
-            title: 'A pizza joke',
-            joke: 'Why did the pizza go to the party? Because it was a little slice of heaven.'
+            title: "A pizza joke",
+            joke: "Why did the pizza go to the party? Because it was a little slice of heaven."
         },
     ];
-
+    
     res.send(jokes);
 });
 
