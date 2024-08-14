56537d8e30315c37fd3ac7c1d567d3ebeddd53dd
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-08-15 00:57:43 +0530

    Project1

--- backend-Dev/Project1/index.js
+++ backend-Dev/Project1/index.js
@@ -10,11 +10,20 @@ app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', function (req, res) {
     fs.readdir(`./files`, function (err, files) {
-        res.render("index",{files: files});
+        res.render("index", { files: files });
+    });
+});
+app.get('/file/:filename', function (req, res) {
+    fs.readFile(`./files/${req.params.filename}`,"utf-8", function (err, filedata) {
+        res.render('show',{filename:req.params.filename, filedata:filedata});
+        console.log(filedata);
     });
 });
 app.post('/create', function (req, res) {
- 
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function (err) {
+        res.redirect('/');
+    });
+
 });
 
 app.listen(5000);
