09fe5b31386a74f81d523aa999ec199f85cf59c6
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-08-26 14:46:55 +0530

    MongoDb

--- backend-Dev/MongoDb/app.js
+++ backend-Dev/MongoDb/app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
-
+const userModel = require('./usermodel');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.get('/create', async (req, res) => {
+    let createduser = await userModel.create({
+        name: "Raunak",
+        username: "Raunak123",
+        email: "raunakraj213@gmail.com"
+    })
+    res.send(createduser);
+})
+
+app.get('/update', async (req, res) => {
+    let updateduser = await userModel.findOneAndUpdate(
+        { username: "Raunak123" },
+        { name: "Raunak Raj" },
+        { new: true }
+    )
+    res.send(updateduser);
+})
 
 app.listen(8000);
--- backend-Dev/MongoDb/usermodel.js
+++ backend-Dev/MongoDb/usermodel.js
@@ -1,9 +1,10 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect(`mongodb://127.0.0.1:27017/mongodbPractice`);
 
-const userSchema=mongoose.Schema({
-    name:String,
-    username:String,
-    email:String
+
+const userSchema = mongoose.Schema({
+    name: String,
+    username: String,
+    email: String
 })
-mongoose.model("user",userSchema);
+module.exports = mongoose.model("user", userSchema);
