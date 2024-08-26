3b12089ea073b011d0d0e63a228fbaf08e0a256c
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-08-26 15:09:23 +0530

    MongoDb

--- backend-Dev/MongoDb/app.js
+++ backend-Dev/MongoDb/app.js
@@ -7,13 +7,17 @@ app.get('/', (req, res) => {
 });
 app.get('/create', async (req, res) => {
     let createduser = await userModel.create({
-        name: "Raunak",
-        username: "Raunak123",
+        name: "RaunakSingh",
+        username: "Raunak1234",
         email: "raunakraj213@gmail.com"
     })
     res.send(createduser);
 })
 
+app.get('/read', async (req, res) => {
+    let users = await userModel.find();
+    res.send(users);
+})
 app.get('/update', async (req, res) => {
     let updateduser = await userModel.findOneAndUpdate(
         { username: "Raunak123" },
@@ -21,6 +25,10 @@ app.get('/update', async (req, res) => {
         { new: true }
     )
     res.send(updateduser);
+})
+app.get('/delete', async (req, res) => {
+    let users = await userModel.findOneAndDelete({ username: "Raunak123" });
+    res.send(users);
 })
 
 app.listen(8000);
